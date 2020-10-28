e522dccdecc0edb60d358b1cc6a9fe7db4212003
Adam Tamargo <tamargoadam@gmail.com>
2020-10-28 17:46:24 -0400

    generate encoded.bin; select heap via constructor

--- src/HuffmanEncoder.java
+++ src/HuffmanEncoder.java
@@ -1,17 +1,22 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Hashtable;
-import java.util.Scanner;
 
 public class HuffmanEncoder implements HuffmanCoding {
 	
+	private String heapType = "binary"; // binary, 4way, or pairing
 	private Hashtable<Character, String> encodeTable = new Hashtable<Character, String>(128);
 	
-	//Search through Huffman tree
+	// constructor
+	// defaults heap type to binary
+	HuffmanEncoder(){}
+	HuffmanEncoder(String heap){
+		heapType = heap;
+	}
+	
+	// search through huffman tree
 	private Hashtable<Character, String> huffEncodeTable(HuffmanTree huff){
 		for(int i=0; i<127; i++){
 				if(characterCode(huff.root(), (char)i, "") != ""){
@@ -22,7 +27,7 @@ public class HuffmanEncoder implements HuffmanCoding {
 	}
 	
 	
-	//Finds each char's code
+	// finds each char's code
 	public static String characterCode(HuffBaseNode bNode, char c, String code){
         HuffInternalNode node = (HuffInternalNode) bNode;
 		if(node!=null){
@@ -39,49 +44,40 @@ public class HuffmanEncoder implements HuffmanCoding {
         return "";
     }
 	
-	//Create Hashtable with ASCII key and frequency value
+	// create hashtable with ASCII key and frequency value
 	private Hashtable<Character, Integer> fileToHash(File inputFile){
 		Hashtable<Character, Integer> charHash = new Hashtable<Character, Integer>(128, 1);
-		FileInputStream file;
-		Scanner s;
+		
+		int c;
+		int newVal;
+		BufferedReader br;
 		try {
-			file = new FileInputStream(inputFile);
-			s = new Scanner(file);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			return null;
-		}
-		
-	    String line;
-	    char readChar;
-	    int newVal;
-		
-	    //Calculates frequencies from scanner
-	    while(s.hasNext()){
-	    	line = s.nextLine();
-	    	for(int i=0; i<line.length(); i++){
-	    		readChar = line.charAt(i);
-	    		//increments value
-	    		if(!charHash.containsKey(readChar)){
-	    			charHash.put(readChar, 1); 
+			br = new BufferedReader(new FileReader(inputFile));
+			while((c=br.read())!=-1)
+			{
+				if(!charHash.containsKey((char)c)){
+	    			charHash.put((char)c, 1); 
 	    		}else{
-	    			newVal = charHash.get(readChar) + 1;
-	    			charHash.replace(readChar, newVal);
+	    			newVal = charHash.get((char)c) + 1;
+	    			charHash.replace((char)c, newVal);
 	    		}
-	    	}
-	    }
-	    s.close();
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	    return charHash;
 	}
 	
 	
-	//Take a file as input and create a table with characters and frequencies
-	//Print the characters and frequencies
+	// take a file as input and create a table with characters and frequencies
+	// print the characters and frequencies
 	public String getFrequencies(File inputFile){
 		
 		Hashtable<Character, Integer> charHash = fileToHash(inputFile);
 	    
-		//Display characters and frequencies
+		// display characters and frequencies
 	    String display = "";
 	    for(int i=0; i<127; i++){
 	    	if(charHash.get((char)i) != null)
@@ -92,67 +88,79 @@ public class HuffmanEncoder implements HuffmanCoding {
 	}
 	
 	
-	//Take a file as input and create a Huffman Tree
+	// take a file as input and create a Huffman Tree
 	public HuffmanTree buildTree(File inputFile){
 		
 		Hashtable<Character, Integer> charHash = fileToHash(inputFile);
 	    
-		//Create max heap
-//		BinaryHeap HHeap = new BinaryHeap(128);
-		PairingHeap HHeap = new PairingHeap();
-//		FourWayHeap HHeap = new FourWayHeap(128);
+		// create min heap
+		MinHeap HHeap;
+		switch(heapType) {
+		  case "4way":
+			  HHeap = new FourWayHeap(128);
+//			  System.out.println("Using four-way heap...");
+			  break;
+		  case "pairing":
+			  HHeap = new PairingHeap();
+//			  System.out.println("Using pairing heap...");
+			  break;
+		  default:
+			  HHeap = new BinaryHeap(128);
+//			  System.out.println("Using binary heap...");
+		}
+		
 		for(int i=0; i<127; i++){
 		    if(charHash.get((char)i) != null){
 		    	HHeap.insert(new HuffmanTree((char)i, charHash.get((char)i)));
 		    }
 		}
 	    
-		//Create Huffman tree from heap
+		// create huffman tree from heap
 		HuffmanTree tmp1 = null;
 		HuffmanTree tmp2 = null; 
 		HuffmanTree tmp3 = null;
 
-		while (HHeap.heapSize() > 1) { // While two items left
+		while (HHeap.heapSize() > 1) { // while two items left
 			tmp1 = HHeap.removeMin();
 	    	tmp2 = HHeap.removeMin();
 	    	tmp3 = new HuffmanTree(tmp1.root(), tmp2.root(), tmp1.weight() + tmp2.weight());
-			HHeap.insert(tmp3);   // Return new tree to heap
+			HHeap.insert(tmp3);   // return new tree to heap
 		}
 		return tmp3; 
 	    
 	}	
 		
-	//Take a file and a HuffTree and encode the file.
-	//Output a string of 1's and 0's representing the file
+	// take a file and a HuffTree and encode the file.
+	// output a string of 1's and 0's representing the file
 	public String encodeFile(File inputFile, HuffmanTree huffTree){
         
-		//Create table of chars and corresponding code
+		// create table of chars and corresponding code
 		Hashtable<Character, String> encodeTable = huffEncodeTable(huffTree);
 		
+		BitOutputStream s = new BitOutputStream("./encoded.bin");
 		StringBuilder encode = new StringBuilder();
-		StringBuilder fileString = new StringBuilder();
-		String line = "";
+		int c;
+		String code;
 		
 		BufferedReader br;
 		try {
 			br = new BufferedReader(new FileReader(inputFile));
-			while((line=br.readLine())!=null)
+			while((c=br.read())!=-1)
 			{
-			    fileString.append(line);
+				code = encodeTable.get((char)c);
+				encode.append(code);
+				s.writeBits(code.length(), Integer.parseInt(code, 2)); // TODO: add end of file indication
 			}
 			br.close();
+			s.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-	    for(int i=0; i<fileString.length(); i++){
-    		encode.append(encodeTable.get(fileString.charAt(i)));
-    	}
-	    
+
         return encode.toString();
 	}	
 		
-	//take a String and HuffTree and output the decoded words
+	// take a string and huffman tree and output the decoded words
 	public String decodeFile(String code, HuffmanTree huffTree){
 		String decoded = "";
 		boolean found = false;
@@ -181,14 +189,14 @@ public class HuffmanEncoder implements HuffmanCoding {
 		return decoded;
 	}	
 		
-	//print the characters and their codes
+	// print the characters and their codes
 	public String traverseHuffmanTree(HuffmanTree huffTree){
 		
 		String tablePrint = "";
-		//Create table of chars and corresponding code
+		// create table of chars and corresponding code
 		Hashtable<Character, String> codeTable = huffEncodeTable(huffTree);
 		
-		//Create string from table
+		// create string from table
 		for(int i=0; i<127; i++){
 			if(codeTable.get((char)i) != null)
 				tablePrint = tablePrint + ((char)i) + " " + codeTable.get((char)i) + "\n";
--- src/compressionTest.java
+++ src/compressionTest.java
@@ -5,17 +5,22 @@ public class compressionTest {
 	public static void main(String[] args) {
 
 		HuffmanEncoder huff = new HuffmanEncoder();
-		File textFile = new File("C:/Users/tamar/dev/workspacejava/HuffmanAlgorithm/myTest.txt");
+		File textFile = new File("./myTest.txt");
+		
 		System.out.println("Character Frequencies:\n-----------------------");
 		System.out.println(huff.getFrequencies(textFile));
+		
 		System.out.println("Character Codes:\n-----------------------");
-		System.out.println(huff.traverseHuffmanTree(huff.buildTree(textFile)));
+		HuffmanTree huffTree = huff.buildTree(textFile);
+		System.out.println(huff.traverseHuffmanTree(huffTree));
+		
 		System.out.println("Encoded File:\n-----------------------");
-		String encoded = huff.encodeFile(textFile, huff.buildTree(textFile));
+		String encoded = huff.encodeFile(textFile, huffTree);
 		System.out.println(encoded + "\n");
 		System.out.println("length:" + encoded.length() + "\n");
+		
 		System.out.println("Decoded File:\n-----------------------");
-		System.out.println(huff.decodeFile(huff.encodeFile(textFile, huff.buildTree(textFile)),huff.buildTree(textFile)) + "\n");
+		System.out.println(huff.decodeFile(encoded, huffTree) + "\n");
 		
 	}
 
