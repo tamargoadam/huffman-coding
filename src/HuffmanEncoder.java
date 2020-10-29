8e3c4410a58b635812e30185e0001ce5c5a13868
Adam Tamargo <tamargoadam@gmail.com>
2020-10-29 16:40:55 -0400

    remove BitInputStream

--- src/HuffmanEncoder.java
+++ src/HuffmanEncoder.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
